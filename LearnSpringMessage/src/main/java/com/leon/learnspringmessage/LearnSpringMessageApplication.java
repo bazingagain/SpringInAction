459842b029b538635575139dd89a540e3149c7a0
bazingagain <1152204424@qq.com>
2018-04-02 23:12:59 +0800

    add spring message

---
+++ LearnSpringMessage/src/main/java/com/leon/learnspringmessage/LearnSpringMessageApplication.java
@@ -0,0 +1,12 @@
+package com.leon.learnspringmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringMessageApplication.class, args);
+	}
+}
