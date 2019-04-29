2f0b22c78b1fd82c415fc976d40a227348292047
bazingagain <1152204424@qq.com>
2019-04-29 23:53:08 +0800

    增加 SpringNeo4j代码

---
+++ Learnneo4j/src/main/java/com/charlie/App.java
@@ -0,0 +1,12 @@
+package com.charlie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Learnneo4j/src/main/java/com/charlie/config/Nei4jConfig.java
@@ -0,0 +1,9 @@
+package com.charlie.config;
+
+
+
+public class Nei4jConfig  {
+    public Nei4jConfig() {
+
+    }
+}
